633d81434a7267d69156a19da94714c2e86900ee
JG-Souza <joaogsouzafma@gmail.com>
2026-04-18 23:28:10 -0300

    Adiciona nanoid para gerar novos códigos às URLs

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 const express = require('express');
+const { nanoid } = require('nanoid');
 const app = express();
 const PORT = 3000;
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-});
+app.post('shorten', (req, res) => {
+    let newUrl = nanoid(6);
+})
+
+app.get(`/${code}`, (req, res) => {
+
+})
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
