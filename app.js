0da9677df620a8a9440fa02e3843240c483d9e5f
JG-Souza <joaogsouzafma@gmail.com>
2026-04-19 00:04:21 -0300

    Implementa lógica base para encurtar URLs e executar direcionamento

--- app.js
+++ app.js
@@ -1,15 +1,43 @@
 const express = require('express');
 const { nanoid } = require('nanoid');
+const Redis = require('ioredis');
+
 const app = express();
 const PORT = 3000;
 
-app.post('shorten', (req, res) => {
-    let newUrl = nanoid(6);
-})
+const redis = new Redis();
 
-app.get(`/${code}`, (req, res) => {
+app.use(express.json());
 
-})
+app.post('/shorten', async (req, res) => {
+    const originalUrl = req.body.url;
+    const newUrl = nanoid(6);
+
+    try {
+        await redis.set(newUrl, originalUrl);
+        res.status(200).json({
+            originalUrl: originalUrl,
+            newUrl: newUrl
+        });
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ error: 'Erro ao salvar no Redis'});
+    }
+});
+
+app.get('/:shortUrl', async (req, res) => {
+    try {
+        const result = await redis.get(req.params.shortUrl);
+            if (result) {
+                res.redirect(`${result}`);
+            } else {
+                res.status(404).json({ error: 'URL não encontrada' });
+            }
+        } catch (err) {
+            console.error(err);
+            res.status(500).json({ error: 'Erro ao buscar URL no Redis' });
+        }
+});
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
